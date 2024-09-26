d0fb057f281f3b42777b3361faa1eca7799775ee
raushan <kraushan022@gmail.com>
2024-09-26 19:16:09 +0530

    sign in and sign up user using firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,11 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidateData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -17,6 +22,49 @@ const Login = () => {
       password.current.value
     );
     setErrorMessage(message);
+
+    if (message) return;
+
+    // sign in/sign up logic
+    if (!isSignInForm) {
+      // Sign up logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          //Signed up
+          const user = userCredential.user;
+          console.log(user);
+          //...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          //...
+          setErrorMessage(errorCode + " - " + errorMessage);
+        });
+    } else {
+      // Sign in logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+          console.log("loggein", user);
+          
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode+" - "+ errorMessage)
+        });
+    }
   };
 
   const toggleSignInForm = () => {
