1e0f98b9f31dafb2d30f89cb197df41fa205366e
raushan <kraushan022@gmail.com>
2024-09-29 23:26:26 +0530

    custom hooks for fetching, storing and using trailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,43 +1,22 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+import React from "react";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../customHooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
-  const trailerVideo = useSelector(store=>store.movies?.trailerVideo)
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   console.log(trailerVideo);
-  
 
-  //fetch trailer video && updating the store with trailer video data 
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/957452/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
+  useMovieTrailer(movieId);
 
-    const filterData = json.results.filter((video) => video?.type == "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log("trailer", trailer);
-
-    //could have stored trailer data in local state vriable also, but using redux to store
-    dispatch(addTrailerVideo(trailer));
-  };
-
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
   return (
     <div>
-      <iframe 
+      <iframe
         width="560"
         height="315"
         src={`https://www.youtube.com/embed/${trailerVideo?.key}`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
+        referrerPolicy="strict-origin-when-cross-origin"
       ></iframe>
     </div>
   );
