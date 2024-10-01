7dd829ac7e85653514515650bcad28caab57bb24
raushan <kraushan022@gmail.com>
2024-10-01 13:58:57 +0530

    fixed browse component showing even after logout

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Login from "./Login";
 import Browse from "./Browse";
+import { useSelector } from "react-redux";
 
 const Body = () => {
-  return (
-    <>
-      <Login />
-      <Browse />
-    </>
-  );
+  const user = useSelector((store) => store.user);
+  return <>{user ? <Browse /> : <Login />}</>;
 };
 
 export default Body;
--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -4,22 +4,18 @@ import useNowPlayingMovies from "../customHooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../customHooks/usePopularMovies";
-import { useSelector } from "react-redux";
 
 const Browse = () => {
-  const user = useSelector((store) => store.user);
   useNowPlayingMovies();
   usePopularMovies();
 
   return (
     <div>
-      {user && (
-        <>
-          <Header />
-          <MainContainer />
-          <SecondaryContainer />
-        </>
-      )}
+      <>
+        <Header />
+        <MainContainer />
+        <SecondaryContainer />
+      </>
     </div>
   );
 };
