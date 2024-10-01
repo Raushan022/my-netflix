ad95d07507a90e7dfb3b0c36aebd51719510692f
raushan <kraushan022@gmail.com>
2024-10-01 11:13:20 +0530

    secondary container with movie list && usePopularMovies in redux

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { IMG_CDN_URL } from '../utils/constants'
+
+const MovieCard = ({posterPath}) => {
+  return (
+    <div className='w-36 pr-3'>
+      <img
+       alt='movie_card' 
+       src={IMG_CDN_URL+posterPath} 
+       />
+    </div>
+  )
+}
+
+export default MovieCard
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,3 +15,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTcxMmM1YTMwYjhkZTc2NjhjYjJlZmFmM2NiYTEwYyIsIm5iZiI6MTcyNzU4OTAyNS4wODQ0MDIsInN1YiI6IjY2ZjhlOThjMGY2ZmEyY2ZjZTVmNWIzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UTLVqbjboVGQiPlXsCzt3PFsWL9ySjJUPvq3XjvdgNw",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
